fd953894cffdf76d8c01a17a9eef8fcf1877a481
nkkn-prog <nkkn.prog@gmail.com>
2025-06-01 01:37:01 +0900

    refactor: トップページをログイン画面に修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Button, Container, Paper, Text, Stack } from '@mantine/core';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import LoginPage from './login/page';
 
 export default function Home() {
   const router = useRouter();
@@ -13,22 +13,6 @@ export default function Home() {
   }, [router]);
   
   return (
-    <Container size="sm" mt="xl">
-      <Paper p="xl" shadow="md" withBorder>
-        <Stack>
-          <Text size="xl" fw={700} ta="center">勤怠管理アプリ</Text>
-          <Text c="dimmed" ta="center">ログインして勤怠管理を始めましょう</Text>
-          
-          <Button 
-            onClick={() => router.push('/dashboard')}
-            fullWidth
-            size="lg"
-            mt="md"
-          >
-            ダッシュボードを表示
-          </Button>
-        </Stack>
-      </Paper>
-    </Container>
+    <LoginPage />
   );
 }
